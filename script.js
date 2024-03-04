5ab0f7e8d64089050407653cc66bd448cd3683d5
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-04 16:09:01 +0530

    minor fixs

--- script.js
+++ script.js
@@ -1,161 +1,87 @@
+
+
 const light = document.querySelector("#light");
 
 $(function() {
-    var b = $("#one");
+  function toggleWrapper(b, w, l) {
+    if (w.hasClass('open')) {
+      w.removeClass('open');
+      w.height(0);
+    } else {
+      // Close any other open wrapper
+      $(".wrapper").not(w).removeClass('open').height(0);
+
+      w.addClass('open');
+      w.height(l.outerHeight(true));
+    }
+  }
+
+  $("#one").click(function() {
     var w = $("#wrapper1");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
 
-
-
-  $(function() {
-    var b = $("#two");
+  $("#two").click(function() {
     var w = $("#wrapper2");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
-  $(function() {
-    var b = $("#three");
+
+  $("#three").click(function() {
     var w = $("#wrapper3");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
-  $(function() {
-    var b = $("#four");
+
+  $("#four").click(function() {
     var w = $("#wrapper4");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
-  $(function() {
-    var b = $("#five");
+
+  $("#five").click(function() {
     var w = $("#wrapper5");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
-  $(function() {
-    var b = $("#six");
+
+  $("#six").click(function() {
     var w = $("#wrapper6");
-    var l = $("#list");
-  
-    
-  
-    b.click(function() {
-  
-      if (w.hasClass('open')) {
-        w.removeClass('open');
-        w.height(0);
-      } else {
-        w.addClass('open');
-        w.height(l.outerHeight(true));
-      }
-  
-    });
+    toggleWrapper($(this), w, $("#list"));
   });
+});
+
+
 let click = 0;
 
 function lightmode() {
-    click++;
-    console.log("light mode clicked", click);
+  click++;
+  console.log("light mode clicked", click);
 
-   
-    const topbar = document.querySelector(".topbar");
-    const background3=document.querySelector("#background3");
+  const topbar = document.querySelector(".topbar");
+  const background3 = document.querySelector("#background3");
 
-
-    
-    if (click % 2 === 1) {
-        topbar.style.backgroundColor = "#ccc"; 
-        background3.style.backgroundColor="#ccc"
-
-    } else {
-        topbar.style.backgroundColor = "#474d56"; 
-        background3.style.backgroundColor="#252a2e"
-
-    }
+  if (click % 2 === 1) {
+    topbar.style.backgroundColor = "#ccc";
+    background3.style.backgroundColor = "#ccc";
+  } else {
+    topbar.style.backgroundColor = "#474d56";
+    background3.style.backgroundColor = "#252a2e";
+  }
 }
 
 light.addEventListener("click", lightmode);
 
+var btc = document.querySelector("#btc");
+var eth = document.querySelector("#eth");
+var doge = document.querySelector("#doge");
 
-var btc=document.querySelector("#btc");
-var eth=document.querySelector("#eth");
-var doge=document.querySelector("#doge");
+var settings = {
+  "async": true,
+  "scrossDomain": true,
+  "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+  "method": "GET",
+  "headers": {}
+};
 
-var settings={
-    "async":true,
-    "scrossDomain":true,
-    "url":"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
-    "method":"GET",
-    "headers":{}
-}
-$.ajax(settings).done(function(response){
-    btc.innerHTML="$"+response.bitcoin.usd;
-    eth.innerHTML="$"+response.ethereum.usd;
-    doge.innerHTML="$"+response.dogecoin.usd;
-})
+$.ajax(settings).done(function(response) {
+  btc.innerHTML = "$" + response.bitcoin.usd;
+  eth.innerHTML = "$" + response.ethereum.usd;
+  doge.innerHTML = "$" + response.dogecoin.usd;
+});
